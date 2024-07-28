bc974ad7845f3a660cb016b7754e29ce89025dfe
Miguel Angel Ramirez Lopez <mikeangeloo87@outlook.com>
2024-07-28 03:36:25 -0500

    adding balancer

--- main.tf
+++ main.tf
@@ -207,3 +207,54 @@ resource "null_resource" "app_setup" {
     null_resource.db_setup
   ]
 }
+
+### Balanceador de carga
+# Creamos el target group para asociar las instancias EC2
+resource "aws_lb_target_group" "app_target_group" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    interval            = 30
+    path                = "/"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+# Creamos el load balancer
+resource "aws_lb" "app_lb" {
+  name               = "app-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.app_sg.id]
+  subnets            = [aws_subnet.app_subnet.id, aws_subnet.db_subnet.id]
+
+  enable_deletion_protection = false
+  idle_timeout = 60
+}
+
+# Creamos el listener
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+  }
+}
+
+# Asociamos las intancias EC2 al target group
+resource "aws_lb_target_group_attachment" "app_instance_attachment" {
+  target_group_arn = aws_lb_target_group.app_target_group.arn
+  target_id        = aws_instance.app_instance.id
+  port             = 80
+}
+
